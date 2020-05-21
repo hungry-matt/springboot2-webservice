979c14265f81e420fea57d1771ec62aace6b0de1
hungry-matt <yoodjin2@gmail.com>
2020-05-22 08:32:02 +0900

    feat: lombok 테스트 검증 메소드 추가

--- src/main/java/com/example/springboot/web/dto/HelloResponseDto.java
+++ src/main/java/com/example/springboot/web/dto/HelloResponseDto.java
@@ -3,7 +3,9 @@ package com.example.springboot.web.dto;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+//선언된 모든 필드의 get 메소드를 생성해 준다.
 @Getter
+//선언된 모든 final 필드를 포함한 생성자 생성
 @RequiredArgsConstructor
 public class HelloResponseDto {
     private final String name;
--- src/test/java/com/example/springboot/web/dto/HelloResponseDtoTest.java
+++ src/test/java/com/example/springboot/web/dto/HelloResponseDtoTest.java
@@ -1,5 +1,23 @@
 package com.example.springboot.web.dto;
 
-public class HelloResponseDtoTest {
+import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+    @Test
+    public void lombokTest() {
+        String name = "name";
+        int amount = 1000;
+
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //assetTaht: assertJ 테스트 검증 라이브러리의 검증 메소드
+        //isEqualTo: assertThat에 있는 값과 비교
+        //Junit을 사용하게 되면 다른라이브러리를 추가로 필요하기 때문에 assertJ 를 사용
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+
+
+    }
 }
