d14215bb8cad16d4458dcc9f97f6e994ff38cf03
hungry-matt <yoodjin2@gmail.com>
2020-06-09 23:48:11 +0900

    add: update 기능

--- src/main/java/com/example/springboot/service/posts/PostsService.java
+++ src/main/java/com/example/springboot/service/posts/PostsService.java
@@ -1,9 +1,14 @@
 package com.example.springboot.service.posts;
 
+import com.example.springboot.domain.posts.Posts;
 import com.example.springboot.domain.posts.PostsRepository;
 import com.example.springboot.domain.posts.PostsSaveRequestDto;
+import com.example.springboot.web.dto.PostsResponseDto;
+import com.example.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 // final 필드가 포함된 생성자를 생성
 @RequiredArgsConstructor
@@ -13,5 +18,21 @@ public class PostsService {
 
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        return new PostsResponseDto(entity);
     }
 }
